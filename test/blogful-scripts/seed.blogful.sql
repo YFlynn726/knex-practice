44bd51ba7a18c61438724c195907304db8f741c0
Yajaira Flynn-Corbin <yary.ba26@gmail.com>
2020-04-04 02:35:59 -0400

    adding test

---
+++ test/blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ('PMSing', '2018-12-02 10:00:00 PST', 'hummus'),
+    ('The maddness', '2016-01-16 12:00:00 PST	', 'Snack')
+    ('Our chaotic world', '2016-05-01 15:00:00 PST	', 'Snack'),
+    ('Earth is falling apart', '2017-02-22 12:00:00 PST	', 'Yo yo yo'),
+    ('Parenting', '2017-04-04 08:00:00 PST	', 'Breakfast'),
+    ('How to mulitask', '2017-04-23 15:00:00 PST	', 'Main'),
+    ('Eat, Pray, Love', '2017-08-11 13:00:00 PST	', 'Main'),
+    ('Dealing with teenagers', '2017-12-09 17:00:00 PST	', 'Breakfast'),
+    ('Being active and healthy', '2018-01-24 19:00:00 PST	', 'Snack'),
+    ('Financial Planning', '2018-01-29 11:00:00 PST	', 'Lunch'),
+    ('Dealing with a toddler', '2018-02-13 05:00:00 PST	', 'Lunch'),
+    ('Being away from home', '2018-03-13 09:00:00 PST	', 'Main'),
+    ('You got friends', '2018-03-31 13:00:00 PST	', 'Lunch'),
+    ('Challenges of adulting', '2019-04-03 07:00:00 PST	', 'Breakfast'),
+    ('Hopes and dreams... crushed', '2019-05-05 21:00:00 PST	', 'Lunch'),
+    ('Learn to dance', '2017-12-09 13:00:00 PST	', 'Breakfast'),
+    ('Need a hobby', '2017-01-29 11:00:00 PST	', 'Main'),
+    ('Fake news', '2016-05-01 15:00:00 PST	', 'Breakfast'),
+    ('Conspiracy theories', '2016-01-13 05:00:00 PST	', 'Snack'),
+    ('Boogie with a hoodie', '2017-08-18 18:00:00 PST	', 'Main')
+    ;
